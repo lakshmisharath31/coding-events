348c6d821db11ed7d7a1493af3031e25583d189e
lakshmisharath31 <lakshmisharath.31@gmail.com>
2020-08-17 14:23:53 -0400

    final

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,53 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class EventCategory {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public EventCategory(){
+
+    }
+
+    public EventCategory( String name) {
+
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventCategory that = (EventCategory) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
