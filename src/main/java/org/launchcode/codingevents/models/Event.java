fa59c35e1c009eafeb4e455624423566ce65dcbd
lakshmisharath31 <62305053+lakshmisharath31@users.noreply.github.com>
2020-08-18 19:44:06 -0400

    Update Event.java

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -8,16 +8,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-/**
- * Created by Chris Bay
- */
+
 @Entity
 public class Event extends AbstractEntity{
-/*
-    @Id
-    @GeneratedValue
-    private int id;
-*/
+
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
@@ -71,28 +65,10 @@ public class Event extends AbstractEntity{
     public void setType(EventType type) {
         this.type = type;
     }
-/*
-    public int getId() {
-        return id;
-    }
-*/
+
     @Override
     public String toString() {
         return name;
     }
-/*
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
- */
 }
