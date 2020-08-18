028420680d2a7859dca1c19bcd983ab8b8c62ad1
lakshmisharath31 <62305053+lakshmisharath31@users.noreply.github.com>
2020-08-18 19:41:59 -0400

    Update EventCategory.java

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -6,16 +6,9 @@ import javax.persistence.Id;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-/**
- * Created by Chris Bay
- */
 @Entity
 public class EventCategory extends AbstractEntity{
 
-
-    //@Id
-    //@GeneratedValue
-   // private int id;
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
@@ -33,28 +26,9 @@ public class EventCategory extends AbstractEntity{
        public void setName(String name) {
         this.name = name;
     }
-/*
-    public int getId() {
-        return id;
-    }
-*/
     @Override
     public String toString() {
         return name;
     }
-/*
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EventCategory that = (EventCategory) o;
-        return id == that.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
- */
 }
